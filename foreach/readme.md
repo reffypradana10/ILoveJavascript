748c2bfd6dae19df9919966ad8d7639c61bc4340
reffypradana10 <reffypradana10@gmail.com>
2024-06-06 22:48:00 +0700

    foreach

---
+++ foreach/readme.md
@@ -0,0 +1,3 @@
+# foEach
+
+forEach adalah sebuah metode pada array di JavaScript yang digunakan untukmengiterasi setiap elemen dalam array tersebut dan menerapkan fungsi (callback) yang telah ditentukan ke setiap elemen.
---
+++ foreach/script.js
@@ -0,0 +1,28 @@
+// forEach is a Method used to iterate to iterate over the elements of an array an d apply a specified function (callback) to each element
+
+// forEach adalah sebuah metode pada array di JavaScript yang digunakan untuk
+// mengiterasi setiap elemen dalam array tersebut dan menerapkan fungsi (callback) yang telah ditentukan ke setiap elemen.
+
+// array.forEach(callback)
+// element, index, array are provided
+
+// let number = [1, 2, 3, 4];
+
+// number.forEach(double);
+// number.forEach(display);
+
+// function double(e, i, arr) {
+//   arr[i] = e * 2;
+// }
+
+// function triple(e, i, arr) {
+//   arr[i] = e * 3;
+// }
+
+// function square(e, i, arr) {
+//   arr[i] = Math.pow(e, 2);
+// }
+
+// function display(e) {
+//   console.log(e);
+// }
